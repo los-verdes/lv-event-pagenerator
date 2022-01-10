78ec718e11625ad4abe2b93c5833150e82dcdfe0
Jeffrey Hogan <jhogan@hashicorp.com>
2022-01-09 18:11:59 -0600

    first TF bits

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,56 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.2.0"
+  hashes = [
+    "h1:2K5LQkuWRS2YN1/YoNaHn9MAzjuTX8Gaqy6i8Mbfv8Y=",
+    "zh:06bd875932288f235c16e2237142b493c2c2b6aba0e82e8c85068332a8d2a29e",
+    "zh:0c681b481372afcaefddacc7ccdf1d3bb3a0c0d4678a526bc8b02d0c331479bc",
+    "zh:100fc5b3fc01ea463533d7bbfb01cb7113947a969a4ec12e27f5b2be49884d6c",
+    "zh:55c0d7ddddbd0a46d57c51fcfa9b91f14eed081a45101dbfc7fd9d2278aa1403",
+    "zh:73a5dd68379119167934c48afa1101b09abad2deb436cd5c446733e705869d6b",
+    "zh:841fc4ac6dc3479981330974d44ad2341deada8a5ff9e3b1b4510702dfbdbed9",
+    "zh:91be62c9b41edb137f7f835491183628d484e9d6efa82fcb75cfa538c92791c5",
+    "zh:acd5f442bd88d67eb948b18dc2ed421c6c3faee62d3a12200e442bfff0aa7d8b",
+    "zh:ad5720da5524641ad718a565694821be5f61f68f1c3c5d2cfa24426b8e774bef",
+    "zh:e63f12ea938520b3f83634fc29da28d92eed5cfbc5cc8ca08281a6a9c36cca65",
+    "zh:f6542918faa115df46474a36aabb4c3899650bea036b5f8a5e296be6f8f25767",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/google" {
+  version = "4.5.0"
+  hashes = [
+    "h1:58TxYMsqoL/7LneWzT/wzXiehf6RtO0UICjqNc71Re8=",
+    "zh:0db8d3e7cc68ddd70a75c72334908b60a875062162c5e8a78bb9e7c0060def64",
+    "zh:2ed5ac0301e6382589353402b11aed8ded08bfad425016a508523e18e8766424",
+    "zh:3abcaf9439bc60b7666cfc257af8bbe555a90b9692972bc46e84b2d96189bf63",
+    "zh:4d9da72347c0bc8dd34fa40ae5c26c7c862dc633e7a5da89564f8fa29d950a49",
+    "zh:5d4d85fb7318df9572e273ee3808c009db7856165a3f1d684e635cec67e4b0ef",
+    "zh:5eac81865b2e1467e2f54e31f8766b098baee9d86e9eeda0f9608f9dbf1863b9",
+    "zh:68c5095136b810a3b80d8aa698252eea4f391500904e4717f5b735f60e04388b",
+    "zh:99c584e796068899cc217d8ecb2204684f851b091a4ab8442e0d9186074f54ca",
+    "zh:a64e291acdd8d61c456073c91abe6b578760ee4526258d416aa5daaa3ed651b3",
+    "zh:acb7844d2a9bac8213764d755fdaeb64e96b50bf11381a66e0300bc4b1aa8040",
+    "zh:f294fd86cfeeee4bef2de31afd7af608d1c7f3f9e35f74127c39b02f58addc4c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version = "0.7.2"
+  hashes = [
+    "h1:NKy1QrNLlP5mKy5Tea6lQSRsVoyydJQKh6WvNTdBF4I=",
+    "zh:0bbe0158c2a9e3f5be911b7e94477586110c51746bb13d102054f22754565bda",
+    "zh:3250af7fd49b8aaf2ccc895588af05197d886e38b727e3ba33bcbb8cc96ad34d",
+    "zh:35e4de0437f4fa9c1ad69aaf8136413be2369ea607d78e04bb68dc66a6a520b8",
+    "zh:369756417a6272e79cad31eb2c82c202f6a4b6e4204a893f656644ba9e149fa2",
+    "zh:390370f1179d89b33c3a0731691e772d5450a7d59fc66671ec625e201db74aa2",
+    "zh:3d12ac905259d225c685bc42e5507ed0fbdaa5a09c30dce7c1932d908df857f7",
+    "zh:75f63e5e1c68e6c5bccba4568c3564e2774eb3a7a19189eb8e2b6e0d58c8f8cc",
+    "zh:7c22a2078a608e3e0278c4cbc9c483909062ebd1843bddaf8f176346c6d378b1",
+    "zh:7cfb3c02f78f0060d59c757c4726ab45a962ce4a9cf4833beca704a1020785bd",
+    "zh:a0325917f47c28a2ed088dedcea0d9520d91b264e63cc667fe4336ac993c0c11",
+    "zh:c181551d4c0a40b52e236f1755cc340aeca0fb5dcfd08b3b1c393a7667d2f327",
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,132 @@
+variable "project_id" {
+  default = "losverdesatx-events"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "service_account_id" {
+  default = "lv-events-page"
+}
+
+variable "service_account_description" {
+  default = "Los Verdes Event Page"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# resource "google_project" "my_project" {
+#   name       = "My Project"
+#   project_id = "your-project-id"
+#   org_id     = "1234567"
+# }
+
+# resource "google_project_service" "project" {
+#   project = "your-project-id"
+#   service = "iam.googleapis.com" # secret manager, drive, cloud run, cloudfunctions?
+
+#   timeouts {
+#     create = "30m"
+#     update = "40m"
+#   }
+
+#   disable_dependent_services = true
+# }
+
+data "archive_file" "webhook_function" {
+  type             = "zip"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/webhook_function.zip"
+  source_dir       = "${path.module}/../webhook/"
+}
+
+
+resource "google_storage_bucket" "bucket" {
+  name     = "lv-event-page"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "functions/webhook_${data.archive_file.webhook_function.output_sha}.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.webhook_function.output_path
+}
+
+resource "google_cloudfunctions_function" "webhook" {
+  name        = "drive-notification-receiver"
+  description = "Listens for calendar-event-related drive changes"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  max_instances         = 5
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "process_push_notification"
+
+  # build_environment_variables = {
+  #   GOOGLE_FUNCTION_SOURCE = "webhook.py"
+  # }
+}
+
+resource "google_cloudfunctions_function_iam_member" "allow_all" {
+  project        = google_cloudfunctions_function.webhook.project
+  region         = google_cloudfunctions_function.webhook.region
+  cloud_function = google_cloudfunctions_function.webhook.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
+resource "google_service_account" "event_page" {
+  account_id   = var.service_account_id
+  display_name = var.service_account_description
+}
+
+# note this requires the terraform to be run regularly
+resource "time_rotating" "event_page_key" {
+  rotation_days = 30
+}
+
+resource "google_service_account_key" "event_page" {
+  service_account_id = google_service_account.event_page.name
+
+  keepers = {
+    rotation_time = time_rotating.event_page_key.rotation_rfc3339
+  }
+}
+
+resource "google_secret_manager_secret" "event_page_key" {
+  secret_id = "event-page-key"
+
+  # labels = {
+  #   service_account_email = google_service_account.event_page.email
+  # }
+
+  replication {
+    automatic = true
+  }
+}
+
+
+resource "google_secret_manager_secret_version" "event_page_key" {
+  secret      = google_secret_manager_secret.event_page_key.id
+  secret_data = google_service_account_key.event_page.private_key
+}
+
+output "event_page_key" {
+  sensitive = true
+  value     = google_service_account_key.event_page.private_key
+}
+
+output "service_account_email" {
+  value = google_service_account.event_page.email
+}
+
+output "secret_version_name" {
+  value = google_secret_manager_secret.event_page_key.name
+}
