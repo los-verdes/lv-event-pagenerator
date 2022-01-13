ba49fdfdd4d83a93834ee715bdfb236ee711420a
Jeffrey Hogan <jhogan@hashicorp.com>
2022-01-13 16:12:59 -0600

    Revert "Dont particular care to keep historical artifacts for this :P"
    This reverts commit e44eb81131fbd4fd71532ceb97b6b2a189fbc143.

--- terraform/cloud_functions.tf
+++ terraform/cloud_functions.tf
@@ -10,8 +10,7 @@ data "archive_file" "webhook_function" {
 }
 
 resource "google_storage_bucket_object" "webhook_archive" {
-  # name   = "terraform/functions/webhook_${data.archive_file.webhook_function.output_sha}.zip"
-  name   = "terraform/functions/webhook.zip"
+  name   = "terraform/functions/webhook_${data.archive_file.webhook_function.output_sha}.zip"
   bucket = data.google_storage_bucket.cloud_functions.name
   source = data.archive_file.webhook_function.output_path
 }
